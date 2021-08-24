a29cf5988aeb6f7766ee85b0189e2bfb21c0a22d
Aric Crosson Bouwers <aricbouwers@outlook.com>
2021-08-24 10:27:30 -0600

    Added 2nd bitmap for crabs, movement logic

---
+++ Crabbypatty.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace seainvaders
+{
+    class Crabbypatty
+    {
+        char[,] var1 =
+                {
+                    { '.','.','0','.','.','.','.','.','0','.','.'},
+                    { '.','.','.','0','.','.','.','0','.','.','.'},
+                    { '.','.','0','0','0','0','0','0','0','.','.'},
+                    { '.','0','0','.','0','0','0','.','0','0','.'},
+                    { '0','0','0','0','0','0','0','0','0','0','0'},
+                    { '0','.','0','0','0','0','0','0','0','.','0'},
+                    { '0','.','0','.','.','.','.','.','0','.','0'},
+                    { '.','.','.','0','0','.','0','0','.','.','.'},
+                };
+
+        char[,] var2 =
+                {
+                    {'.','.','0','.','.','.','.','.','0','.','.'},
+                    {'0','.','.','0','.','.','.','0','.','.','0'},
+                    {'0','.','0','0','0','0','0','0','0','.','0'},
+                    {'0','0','0','.','0','0','0','.','0','0','0'},
+                    {'0','0','0','0','0','0','0','0','0','0','0'},
+                    {'.','0','0','0','0','0','0','0','0','0','.'},
+                    {'.','.','0','.','.','.','.','.','0','.','.'},
+                    {'.','0','.','.','.','.','.','.','.','0','.'},
+                };
+
+        public Bitmap spriteArmsDown;
+        public Bitmap spriteArmsUp;
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+        public bool activeSprite;
+        public int xMove = 2;
+        public int yMove = 8;
+
+        public Crabbypatty(int x, int y)
+        {
+            width = 11;
+            height = 8;
+            this.x = x;
+            this.y = y;
+            spriteArmsDown = new Bitmap(width, height);
+            spriteArmsUp = new Bitmap(width, height);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (var1[i, j] == '.')
+                    {
+                        spriteArmsDown.SetPixel(j, i, Color.Transparent);
+                    }
+                    else
+                    {
+                        spriteArmsDown.SetPixel(j, i, Color.DarkSeaGreen);
+                    }
+                }
+            }
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (var2[i, j] == '.')
+                    {
+                        spriteArmsUp.SetPixel(j, i, Color.Transparent);
+                    }
+                    else
+                    {
+                        spriteArmsUp.SetPixel(j, i, Color.DarkSeaGreen);
+                    }
+                }
+            }
+
+
+        }
+        public bool Move(bool leftToRight)
+        {
+            
+            activeSprite = !activeSprite;
+            if (leftToRight)
+            {
+                x += xMove;
+            }
+            else
+            {
+                x -= xMove;
+            }
+            if( x <= 0)
+            {
+                return true;
+            }
+            if (x >= 208)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public void Render(Graphics graphics)
+        {
+            /*
+             sprite.SetResolution(16, 12);*/
+            if (activeSprite)
+            {
+                graphics.DrawImage(spriteArmsDown, x, y);
+            }
+            else
+            {
+                graphics.DrawImage(spriteArmsUp, x, y);
+
+            }
+        }
+    }
+}
+
+
+
--- Enemyblob.cs
+++ Enemyblob.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace seainvaders
@@ -11,20 +7,23 @@ namespace seainvaders
     class Enemyblob
     {
  
-        public List <Crabbiepatty> crabs;
+        public List <Crabbypatty> crabs;
         public int startingx = 16;
         public int startingy = 120;
+        public int frameCount = 0;
+        public bool leftToRight;
+        public bool moveDown;
         
 
         public Enemyblob()
         {
-            crabs = new List<Crabbiepatty>();
+            crabs = new List<Crabbypatty>();
             for (int i = 0; i < 5; i++)
             {
                 startingx = 16;
                 for (int j = 0; j < 11; j++)
                 {
-                    crabs.Add(new Crabbiepatty(startingx, startingy));
+                    crabs.Add(new Crabbypatty(startingx, startingy));
                     
                     startingx += 16;
                     
@@ -32,15 +31,39 @@ namespace seainvaders
                 startingy += 16;
             }
         }
+        public void Update()
+        {
+            if(crabs.Count > 0)
+            {
+                
+                int move = frameCount % crabs.Count;
+                bool hitWall = crabs[move].Move(leftToRight);
+                if (hitWall)
+                {
+                    moveDown = true;
+                }
+                if (move == crabs.Count - 1 && moveDown)
+                {
+                    foreach(Crabbypatty crab in crabs)
+                    {
+                        crab.y += crab.yMove;
+                    }
+                    moveDown = false;
+                    leftToRight = !leftToRight;
+                }
+                frameCount++;
+
+            }
+        }
 
         public void Render(Graphics graphics)
         {
-           //*
+           //* //this is the same code as below
            // for(int i = 0; i < crabs.Count; i++)
            // {
            //     crabs[i].Render(graphics);
            // }
-           foreach(Crabbiepatty crab in crabs)
+           foreach(Crabbypatty crab in crabs)
             {
                 crab.Render(graphics);
             }
